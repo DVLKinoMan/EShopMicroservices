5b381c7a46d07fc45538294f4cc4052141983d8a
Daviti Dvali <dvali.daviti@gmail.com>
2024-04-23 20:36:58 +0400

    GetProductById Implementation

---
+++ src/Services/Catalog/Catalog.Api/Products/GetProductById/GetProductByIdQueryHandler.cs
@@ -0,0 +1,23 @@
+﻿namespace Catalog.Api.Products.GetProductById
+{
+    public record GetProductByIdQuery(Guid Id) : IQuery<GetProductByIdResult>;
+    public record GetProductByIdResult(Product product);
+
+    internal class GetProductByIdQueryHandler(IDocumentSession session, ILogger<GetProductByIdQueryHandler> logger)
+        : IQueryHandler<GetProductByIdQuery, GetProductByIdResult>
+    {
+        public async Task<GetProductByIdResult> Handle(GetProductByIdQuery query, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("GetProductByIdQueryHandler.Handle called with {@Query}", query);
+
+            var response = await session.LoadAsync<Product>(query.Id, cancellationToken);
+
+            if (response == null)
+            {
+                throw new ProductNotFoundException();
+            }
+
+            return new GetProductByIdResult(response);
+        }
+    }
+}
