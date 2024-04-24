72ecc4a83866f552fa6a79ddfc0e4449ac5e1ff2
Daviti Dvali <dvali.daviti@gmail.com>
2024-04-24 16:33:25 +0400

    StoreBasket Implementation

---
+++ src/Services/Basket/Basket.Api/Basket/StoreBasket/StoreBasketEndpoint.cs
@@ -0,0 +1,28 @@
+﻿
+namespace Basket.Api.Basket.StoreBasket
+{
+    public record StoreBasketRequest(ShoppingCart Cart) : ICommand<StoreBasketResult>;
+    public record StoreBasketResponse(string UserName);
+
+    public class StoreBasketEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPost("/basket", async (StoreBasketRequest request, ISender sender) =>
+            {
+                var command = request.Adapt<StoreBaskCommand>();
+
+                var result = await sender.Send(command);
+
+                var response = result.Adapt<StoreBasketResponse>();
+
+                return Results.Created($"/basket/{response.UserName}", response); 
+            })
+            .WithName("StoreBasket")
+            .Produces<StoreBasketResponse>(StatusCodes.Status201Created)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Create Basket")
+            .WithDescription("Create Basket");
+        }
+    }
+}
---
+++ src/Services/Basket/Basket.Api/Basket/StoreBasket/StoreBasketHandler.cs
@@ -0,0 +1,24 @@
+﻿namespace Basket.Api.Basket.StoreBasket
+{
+    public record StoreBaskCommand(ShoppingCart Cart) : ICommand<StoreBasketResult>;
+    public record StoreBasketResult(string UserName);
+
+    public class StoreBasketCommandvalidator : AbstractValidator<StoreBaskCommand>
+    {
+        public StoreBasketCommandvalidator() 
+        {
+            RuleFor(x => x.Cart).NotNull().WithMessage("Cart can not be null");
+            RuleFor(x => x.Cart.UserName).NotEmpty().WithMessage("UserName is required");
+        }
+    }
+
+    public class StoreBasketHandler : ICommandHandler<StoreBaskCommand, StoreBasketResult>
+    {
+        public async Task<StoreBasketResult> Handle(StoreBaskCommand command, CancellationToken cancellationToken)
+        {
+            var cart = command.Cart;
+
+            return new StoreBasketResult(command.Cart.UserName);
+        }
+    }
+}
