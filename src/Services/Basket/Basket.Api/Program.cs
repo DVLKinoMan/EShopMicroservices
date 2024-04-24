5669fad1d4637f93e36ae33f32620c337459fd38
Daviti Dvali <dvali.daviti@gmail.com>
2024-04-24 16:32:58 +0400

    GetBasket Implementation

--- src/Services/Basket/Basket.Api/Program.cs
+++ src/Services/Basket/Basket.Api/Program.cs
@@ -1,5 +1,19 @@
 var builder = WebApplication.CreateBuilder(args);
 
+var assembly = typeof(Program).Assembly;
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(assembly);
+    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+});
+builder.Services.AddCarter();
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+
 var app = builder.Build();
+
+app.MapCarter();
+
+app.UseExceptionHandler();
 
 app.Run();
