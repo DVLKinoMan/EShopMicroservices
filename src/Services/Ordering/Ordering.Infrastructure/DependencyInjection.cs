5c270df74adcf423c6edb213ee2f6ca313bd57f1
Daviti Dvali <dvali.daviti@gmail.com>
2024-04-26 21:19:19 +0400

    AuditableEntityInterceptor

--- src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
@@ -6,12 +6,15 @@ namespace Ordering.Infrastructure
     public static class DependencyInjection
     {
         public static IServiceCollection AddInfrastructureServices
-            (this IServiceCollection services, 
+            (this IServiceCollection services,
             IConfiguration configuration)
         {
             var connectionString = configuration.GetConnectionString("Database");
             services.AddDbContext<ApplicationDbContext>(options =>
-                                                            options.UseSqlServer(connectionString));
+            {
+                options.AddInterceptors(new AuditableEntityInterceptor());
+                options.UseSqlServer(connectionString);
+            });
 
             return services;
         }
