bf50d19925712e15f777b47eb8e7a551b5474016
Daviti Dvali <dvali.daviti@gmail.com>
2024-04-27 17:36:21 +0400

    DeleteOrder Enpoint

--- src/Services/Ordering/Ordering.Api/Endpoints/DeleteOrder.cs
+++ src/Services/Ordering/Ordering.Api/Endpoints/DeleteOrder.cs
@@ -1,6 +1,30 @@
-﻿namespace Ordering.Api.Endpoints
+﻿using Ordering.Application.Orders.Commands.DeleteOrder;
+
+namespace Ordering.Api.Endpoints
 {
-    public class DeleteOrder
+    //public record DeleteOrderRequest(Guid id);
+
+    public record DeleteOrderResponse(bool IsSuccess);
+
+    public class DeleteeOrder : ICarterModule
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapDelete("/orders/{id}", async (Guid id, ISender sender)
+                =>
+            {
+                var result = await sender.Send(new DeleteOrderCommand(id));
+
+                var response = result.Adapt<DeleteOrderResponse>();
+
+                return Results.Ok(response);
+            })
+            .WithName("DeleteOrder")
+            .Produces<DeleteOrderResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .ProducesProblem(StatusCodes.Status404NotFound)
+            .WithSummary("Delete Order")
+            .WithDescription("Delete Order"); ;
+        }
     }
 }
