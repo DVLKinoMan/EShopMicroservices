c10051a1b1c2e9b443ce5befccc01a846a50d052
Daviti Dvali <dvali.daviti@gmail.com>
2024-04-29 16:21:18 +0400

    IBasketServier WebApp

--- src/WebApps/Shopping.Web/Services/ICatalogService.cs
+++ src/WebApps/Shopping.Web/Services/ICatalogService.cs
@@ -4,7 +4,7 @@
     {
         [Get("/catalog-service/products?pageNumber={pageNumber}&pageSize={pageSize}")]
         Task<GetProductsResponse> GetProducts(int? pageNumber = 1, int? pageSize = 10);
-        [Get("/catalog-service/product{category}")]
+        [Get("/catalog-service/products/{category}")]
         Task<GetProductByCategoryResponse> GetProductsByCategory(string category);
         [Get("/catalog-service/products/{id}")]
         Task<GetProductByIdResponse> GetProduct(Guid id);
